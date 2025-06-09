bd80aed277c237a7a87c7c769ba13bbbcf54e884
Guillaume Gozillon <guillaumegozillon@gmail.com>
2025-06-09 16:26:28 +0200

    chore(setup): install tailwind + prettier with nuxt

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
+
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap'
+    }
+  ]
+})
+</script>
+
+<style>
+html {
+  font-family: 'DM Sans', sans-serif;
+}
+</style>
